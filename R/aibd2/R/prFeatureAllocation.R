c32bb1074411ef207f29b2bc969a2ea7483528be
Jeremy Meyer <jeremymeyer23@hotmail.com>
2018-09-27 11:29:59 -0600

    More pmf updates. Just need to fix Kh term and extract mass parameter from dist.

--- R/aibd2/R/prFeatureAllocation.R
+++ R/aibd2/R/prFeatureAllocation.R
@@ -14,6 +14,8 @@
 #' d <- ibp(2,nrow(fa))
 #' prFeatureAllocation(fa,d)
 
+d <- ipb(2, 5)
+
 Z <- matrix(c(1,0,0,
               1,0,1,
               0,1,1,
@@ -30,28 +32,37 @@ prFeatureAllocation <- function(featureAllocation, distribution, log=FALSE, lof=
   lof_Z <- featureAllocation[,order(apply(featureAllocation, 2,
               function(x) sum(2^((N-1):0)*x)), decreasing = TRUE)]
 
-  k1 <- tabulate(apply(lof_Z, 2, function(x) which(x == 1)[1]))
-  k1fac <- sum(lfactorial(k1))
   HN <- sum(1/1:N)
   mk <- apply(featureAllocation, 2, sum)
 
-  # pmf log scale
-  lpmf <- K*log(alpha)-k1fac-alpha*HN*sum(lfactorial(N-mk) + lfactorial(mk-1) - lfactorial(N))
+
+  if (lof){
+    Kh <- tabulate(apply(lof_Z,2,sum))
+    khfac <- sum(lfactorial(Kh)) # #Why did they sum from to 2^N-1? Start at zero or 2^N-1?
+    lpmf <- -khfac + K*log(alpha)-alpha*HN*sum(lfactorial(N-mk) + lfactorial(mk-1) - lfactorial(N))
+  }
+  else{
+    k1 <- tabulate(apply(lof_Z, 2, function(x) which(x == 1)[1]))
+    k1fac <- sum(lfactorial(k1))
+    lpmf <- K*log(alpha)-k1fac-alpha*HN*sum(lfactorial(N-mk) + lfactorial(mk-1) - lfactorial(N))
+  }
   if ( log ) lpmf else exp(lpmf)
 
 
 }
 # Report: lof matrices do in fact use the binary rule.
-#
+# Question: Why is Kh summing over 2^N-1
 
 
 # First get it to work!
-# Will the Distance matrix make a difference in the feature allocation?
-# Later: Do we care about left ordered form? Assuming this is fine for now...
+# Will the Distance matrix make a difference in the feature allocation? (Not for AIBD)
+# Later: Look at how customers resample dishes
+
 
 # Enumerate all possible feature allocations for <=5 features, set mass parameter to something low
 # Compare with old aibd method for accuracy
 # Think about efficiency. Run testimes to see what's faster
+# Thought: What happens with Zeros?
 
 
 # Function for evaluating number of new dishes per customer
