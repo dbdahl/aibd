04cdaebbb1941c04416ca5b222c6b1ec271f05c2
Jeremy Meyer <jeremymeyer23@hotmail.com>
2018-09-30 17:58:00 -0600

    Cleaned up code. Might have fixed pmf. Accidentlally multiplied instead of added.

--- R/aibd2/R/prFeatureAllocation.R
+++ R/aibd2/R/prFeatureAllocation.R
@@ -5,6 +5,7 @@
 #' @param featureAllocation test
 #' @param distribution A feature allocation distribution
 #' @param log Should results be given on the log scale?
+#' @param lof Should the probability be given on the left ordered form feature allocation?
 #'
 #' @return The probability (or log of the probability) of the given feature allocation.
 #' @export
@@ -14,31 +15,31 @@
 #' d <- ibp(2,nrow(fa))
 #' prFeatureAllocation(fa,d)
 
-d <- ibp(2, 5)
+distribution <- ibp(2, 5)
 
-Z <- matrix(c(1,0,0,
-              1,0,1,
-              0,1,1,
-              0,0,1,
-              1,1,0), ncol=3, byrow = TRUE)
+Z <- matrix(c(1,0,0,0,
+              1,0,1,1,
+              0,1,1,1,
+              0,0,1,1,
+              1,1,0,0), ncol=4, byrow = TRUE)
 
-prFeatureAllocation <- function(featureAllocation, distribution, log=FALSE, lof.prob=TRUE) {
+prFeatureAllocation <- function(featureAllocation, distribution, log=FALSE, lof=TRUE) {
   if ( !inherits(distribution,"ibpFADistribution") ) stop("Unsupported distribution.")
 
   N <- nrow(featureAllocation)
   K <- ncol(featureAllocation)
   alpha <- distribution$mass
 
-  lof_Z <- featureAllocation[,order(apply(featureAllocation, 2,
-              function(x) sum(2^((N-1):0)*x)), decreasing = TRUE)]
+  binary_nums <- apply(featureAllocation, 2, function(x) sum(2^((N-1):0)*x))
+  lof_Z <- featureAllocation[,order(binary_nums, decreasing = TRUE)]
 
   HN <- sum(1/1:N)
   mk <- apply(featureAllocation, 2, sum)
 
 
-  if (lof.prob){
+  if (lof){
     Kh <- tabulate(apply(lof_Z,2,sum))
-    khfac <- sum(lfactorial(Kh)) # #Why did they sum from to 2^N-1? Start at zero or 2^N-1?
+    khfac <- sum(lfactorial(table(binary_nums)))
     lpmf <- -khfac + K*log(alpha)-alpha*HN*sum(lfactorial(N-mk) + lfactorial(mk-1) - lfactorial(N))
   }
   else{
@@ -51,6 +52,7 @@ prFeatureAllocation <- function(featureAllocation, distribution, log=FALSE, lof.
 }
 # Report: lof matrices do in fact use the binary rule.
 # Question: Why is Kh summing over 2^N-1
+# Fix a Z with no features
 
 
 # First get it to work!
@@ -62,13 +64,3 @@ prFeatureAllocation <- function(featureAllocation, distribution, log=FALSE, lof.
 # Compare with old aibd method for accuracy
 # Think about efficiency. Run testimes to see what's faster
 # Thought: What happens with Zeros?
-
-
-# Function for evaluating number of new dishes per customer
-k1 <- numeric(nrow(Z))
-tabulate(apply(lof_Z, 2, function(x) which(x == 1)[1]))
-
-
-
-
-lof_Z = Z[,order(apply(Z, 2, function(x) sum(2^((nrow(Z)-1):0)*x)), decreasing = TRUE)]
