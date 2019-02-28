38b131d090903d577007644705b990c1acec733c
David B. Dahl <dahl@stat.byu.edu>
2019-02-28 11:00:37 -0700

    logLikelihoodLGLFM and logPosteriorLGLFM is now tested.

--- R/aibd2/tests/testthat/test-lglfm-likelihood.R
+++ R/aibd2/tests/testthat/test-lglfm-likelihood.R
@@ -2,21 +2,29 @@ context("lglfm-likelihood")
 
 # skip("lglfm-likelihood")
 
+mass <- 1.0
+sigx <- 0.1
+sigw <- 1.0
+dimW <- 1
+nItems <- 4  # Should be a multiple of 4
+Z <- matrix(c(1,0,1,1,0,1,0,0),byrow=TRUE,nrow=nItems,ncol=2)
+Z <- Z[order(Z %*% c(2,1)),c(2,1)]
+Ztruth <- Z
+W <- matrix(rnorm(ncol(Z)*dimW,sd=sigw),nrow=ncol(Z),ncol=dimW)
+e <- rnorm(nrow(Z)*ncol(W),0,sd=sigx)
+X <- Z %*% W + e
+maxNFeatures <- 6
+featureAllocations <- enumerateFeatureAllocations(nItems, maxNFeatures)
+
 test_that("R and Scala give the same values for likelihood in LGLFM", {
-  mass <- 1.0
-  sigx <- 0.1
-  sigw <- 1.0
-  dimW <- 1
-  nItems <- 4  # Should be a multiple of 4
-  Z <- matrix(c(1,0,1,1,0,1,0,0),byrow=TRUE,nrow=nItems,ncol=2)
-  Z <- Z[order(Z %*% c(2,1)),c(2,1)]
-  Ztruth <- Z
-  W <- matrix(rnorm(ncol(Z)*dimW,sd=sigw),nrow=ncol(Z),ncol=dimW)
-  e <- rnorm(nrow(Z)*ncol(W),0,sd=sigx)
-  X <- Z %*% W + e
-  maxNFeatures <- 6
-  featureAllocations <- enumerateFeatureAllocations(nItems, maxNFeatures)
-  logLikeFromR     <- logLikelihoodLGLFM(featureAllocations, X, sdX=sigx, sdW=sigw, implementation="R")
-  logLikeFromScala <- logLikelihoodLGLFM(featureAllocations, X, sdX=sigx, sdW=sigw, implementation="scala")
-  expect_equal(logLikeFromR, logLikeFromScala)
+  logLikelihoodFromR     <- logLikelihoodLGLFM(featureAllocations, X, sdX=sigx, sdW=sigw, implementation="R")
+  logLikelihoodFromScala <- logLikelihoodLGLFM(featureAllocations, X, sdX=sigx, sdW=sigw, implementation="scala")
+  expect_equal(logLikelihoodFromR, logLikelihoodFromScala)
+})
+
+test_that("R and Scala give the same values for posterior in LGLFM", {
+  dist <- ibp(mass, nItems)
+  logPosteriorFromR     <- logPosteriorLGLFM(featureAllocations, dist, X, sdX=sigx, sdW=sigw, implementation="R")
+  logPosteriorFromScala <- logPosteriorLGLFM(featureAllocations, dist, X, sdX=sigx, sdW=sigw, implementation="scala")
+  expect_equal(logPosteriorFromR, logPosteriorFromScala)
 })
