1b6e544bee52bce43f75580fc831a83d2c0c07d0
David B. Dahl <dahl@stat.byu.edu>
2019-03-13 17:20:30 -0600

    Yes

---
+++ experimental/lglfm/src/main/scala/org/ddahl/lglfm/FeatureAllocation.scala
@@ -0,0 +1,112 @@
+package org.ddahl.lglfm
+
+import breeze.linalg._
+
+object FeatureAllocation {
+
+  def apply(z: Array[Array[Double]]): DenseMatrix[Double] = {
+    DenseMatrix.create(z.size, z(0).size, z.flatten, 0, z(0).size, true)
+  }
+
+  def apply(z: Array[Array[Int]]): DenseMatrix[Double] = {
+    apply(z.map(_.map(_.toDouble)))
+  }
+
+  def apply(z: Array[Array[Boolean]]): DenseMatrix[Double] = {
+    apply(z.map(_.map(x => if ( x ) 1.0 else 0.0)))
+  }
+
+  def isValid(Z: DenseMatrix[Double]): Boolean = {
+    Z.forall(x => ( x == 0.0 ) || ( x == 1.0 ) )
+  }
+
+  def toFingerprint(Z: DenseMatrix[Double]): Array[BigInt] = {
+    val coefficients = Array.fill[BigInt](Z.rows-1)(BigInt(2)).scan(BigInt(1))(_*_)
+    val result = Array.fill(Z.cols+1)(BigInt(0))
+    result(0) = Z.rows
+    var j = 0
+    while ( j < Z.cols ) {
+      var sum = result(j+1)
+      val a = Z(::,j).map(_ != 0.0)
+      var i = 0
+      while ( i < Z.rows ) {
+        if ( a(i) ) sum += coefficients(Z.rows-i-1)
+        i += 1
+      }
+      j += 1
+      result(j) = sum
+    }
+    result
+  }
+
+  def toFingerprintString(Z: DenseMatrix[Double]): String = {
+    toFingerprint(Z).mkString(",")
+  }
+
+  def fromFingerprint(x: Array[BigInt]): DenseMatrix[Double] = {
+    val Z = DenseMatrix.zeros[Double](x(0).toInt, x.length-1)
+    var j = 0
+    while ( j < Z.cols ) {
+      val w = x(j+1)
+      var i = 0
+      while ( i < Z.rows ) {
+        Z(i,j) = if ( (w & (1<<i)) != 0 ) 1.0 else 0.0
+        i += 1
+      }
+      j += 1
+    }
+    Z
+  }
+
+  def fromFingerprintString(x: String): DenseMatrix[Double] = {
+    fromFingerprint(x.split(",").map(BigInt.apply))
+  }
+
+ def leftOrderedForm(Z: DenseMatrix[Double]): Matrix[Double] = {
+    val fp = toFingerprint(Z)
+    val order = (0 until Z.cols).zip(fp.view(1,Z.cols+1)).sortWith( _._2 > _._2 ).map(_._1)
+    Z(::,order).toDenseMatrix
+  }
+
+  def partitionBySingletonsOf(i: Int, Z: DenseMatrix[Double]): (Matrix[Double],Matrix[Double]) = {
+    val a = Z(::,*).map { f =>
+      ( f(i) != 0.0 ) && ( sum(f) == 1.0 )
+    }
+    (Z(::,a.t), Z(::,!a.t))
+  }
+
+  def enumerateCombinationsFor(i: Int, Z: DenseMatrix[Double]): List[DenseMatrix[Double]] = {
+    if ( Z.cols > 31 ) throw new IllegalArgumentException("No more than 31 columns are supported.")
+    val max = (math.pow(2, Z.cols) - 1).toInt
+    var result = List[DenseMatrix[Double]]()
+    var w = 0
+    while ( w <= max ) {
+      val newZ = Z.copy
+      newZ(i,::) := DenseVector(Array.tabulate(Z.cols)(j => if ( (w & (1<<j)) != 0 ) 1.0 else 0.0)).t
+      result = newZ :: result
+      w += 1
+    }
+    result
+  }
+
+  def main(args: Array[String]): Unit = {
+    val m = Array(Array(0,1,0,1),Array(1,0,1,0),Array(1,0,1,0),Array(0,0,0,1),Array(1,0,0,0))
+    val Z = apply(m)
+    println(isValid(Z))
+    Z(0,2) = 5.0
+    println(isValid(Z))
+    Z(0,2) = 1.0
+    println(Z)
+    println("---")
+    println(leftOrderedForm(Z))
+    println(partitionBySingletonsOf(0,Z))
+    println(enumerateCombinationsFor(0,Z).mkString("\n---------\n"))
+
+    // val part = partitionBySingletonsOf(0,Z)
+    // val Zempty = part._2
+    // Zempty(0,::) = DenseVector.zeros[Double](Z.cols).t
+    // val lc = new LinearGaussianLatentFeatureModel(X,1,1)
+    println(enumerateCombinationsFor(0,Z).map(ZZ => toFingerprintString(ZZ)))
+  }
+
+}
--- experimental/lglfm/src/main/scala/org/ddahl/lglfm/LinearGaussianLatentFeatureModel.scala
+++ experimental/lglfm/src/main/scala/org/ddahl/lglfm/LinearGaussianLatentFeatureModel.scala
@@ -3,7 +3,7 @@ package org.ddahl.lglfm
 import breeze.linalg._
 import org.apache.commons.math3.util.FastMath.log
 
-class LinearGaussianLatentFeatureModel private (X: DenseMatrix[Double], precisionX: Double, precisionW: Double) {
+class LinearGaussianLatentFeatureModel private (val X: DenseMatrix[Double], val precisionX: Double, val precisionW: Double) {
 
   val N = X.rows
   val D = X.cols
@@ -13,22 +13,73 @@ class LinearGaussianLatentFeatureModel private (X: DenseMatrix[Double], precisio
   private val Dhalf = D / 2.0
   private val const = -N * Dhalf * log(2 * math.Pi)
   private val XtXtrace = trace(Xt * X)
+  private val DhalfTimeslogPrecisionX = Dhalf * log(precisionX)
+  private val halfPrecisionX = precisionX / 2
+  private val logPrecisionW = log(precisionW)
+  private val ratioOfPrecisionsTimesI = I *:* precisionW / precisionX
 
-  def logLikelihood(Z: DenseMatrix[Double]): Double = {
-    if (Z.rows != N) throw new IllegalArgumentException("Feature allocation has " + Z.rows + " items, but " + N + " were expected.")
-    val K = Z.cols
-    if (K == 0) {
-      const + (N) * Dhalf * log(precisionX) + -precisionX / 2 * XtXtrace
+  def logLikelihood(lc: LikelihoodComponents): Double = {
+    if (lc.K == 0) {
+      const + (N) * DhalfTimeslogPrecisionX - halfPrecisionX * XtXtrace
     } else {
-      val Zt = Z.t
-      val ZtZplusRatioI = Zt * Z + I *:* (precisionW / precisionX)
-      const + (N - K) * Dhalf * log(precisionX) + K * Dhalf * log(precisionW) - Dhalf * log(det(ZtZplusRatioI)) - precisionX / 2 * trace(Xt * (I - Z * inv(ZtZplusRatioI) * Zt) * X)
+      const + (N - lc.K) * DhalfTimeslogPrecisionX + lc.K * Dhalf * logPrecisionW + Dhalf * log(det(lc.M)) - halfPrecisionX * trace(Xt * (I - lc.Z * lc.M * lc.Zt) * X)
     }
+  }
+
+  def logLikelihood(Z: DenseMatrix[Double]): Double = logLikelihood(computeLikelihoodComponents(Z))
+
+  class LikelihoodComponents private[LinearGaussianLatentFeatureModel] (val Z: DenseMatrix[Double], val Zt: DenseMatrix[Double], val M: DenseMatrix[Double]) {
+    val K = Z.cols
+  }
+
+  def computeLikelihoodComponents(Z: DenseMatrix[Double]) = {
+    if (Z.rows != N) throw new IllegalArgumentException("Feature allocation has " + Z.rows + " items, but " + N + " were expected.")
+    val Zt = Z.t
+    val M = inv(Zt * Z + ratioOfPrecisionsTimesI)
+    new LikelihoodComponents(Z, Zt, M)
+  }
+
+  private def update(M: DenseMatrix[Double], z: Transpose[DenseVector[Double]], add: Boolean) = {
+    val zt = z.t
+    M - M * zt * z * M /:/ (z * M * zt + ( if (add) 1 else -1))
+  }
+
+  def dropFeaturesFor(i: Int, lc: LikelihoodComponents): LikelihoodComponents = {
+    val Z = lc.Z.copy
+    val zOld = Z(i,::)
+    Z(i,::) := DenseVector.zeros[Double](lc.K).t
+    new LikelihoodComponents(Z, Z.t, update(lc.M, zOld, false))
+  }
+
+  def addFeaturesFor(i: Int, lc: LikelihoodComponents, z: Transpose[DenseVector[Double]]): LikelihoodComponents = {
+    val Z = lc.Z.copy
+    // assert(!any(Z(i,::)))
+    Z(i,::) := z
+    new LikelihoodComponents(Z, Z.t, update(lc.M, z, true))
+  }
+
+  def dropAndAddFeaturesFor(i: Int, lc: LikelihoodComponents, z: Transpose[DenseVector[Double]]): LikelihoodComponents = {
+    val newLC = dropFeaturesFor(i, lc)
+    newLC.Z(i,::) := z
+    new LikelihoodComponents(newLC.Z, newLC.Z.t, update(newLC.M, z, true))
   }
 
 }
 
+
 object LinearGaussianLatentFeatureModel {
+
+  def usingPrecisions(X: DenseMatrix[Double], precisionX: Double, precisionW: Double): LinearGaussianLatentFeatureModel = {
+    new LinearGaussianLatentFeatureModel(X.copy, precisionX, precisionW)
+  }
+
+  def usingStandardDeviations(X: DenseMatrix[Double], standardDeviationX: Double, standardDeviationW: Double): LinearGaussianLatentFeatureModel = {
+    new LinearGaussianLatentFeatureModel(X.copy, 1/(standardDeviationX*standardDeviationX), 1/(standardDeviationW*standardDeviationW))
+  }
+
+  def usingVariances(X: DenseMatrix[Double], varianceX: Double, varianceW: Double): LinearGaussianLatentFeatureModel = {
+    new LinearGaussianLatentFeatureModel(X.copy, 1/varianceX, 1/varianceW)
+  }
 
   def main(args: Array[String]): Unit = {
     val Z = DenseMatrix.zeros[Int](6,2)
