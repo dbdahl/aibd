a0cbfe885e1cdf09a6c29fac5eade05c440900cd
David B. Dahl <dahl@stat.byu.edu>
2019-03-21 23:23:05 -0600

    Yes

--- src/main/scala/org/ddahl/aibd/model/lineargaussian/FeatureAllocationDistributions.scala
+++ src/main/scala/org/ddahl/aibd/model/lineargaussian/FeatureAllocationDistributions.scala
@@ -10,7 +10,7 @@ object FeatureAllocationDistributions {
     if ( fa.nFeatures == 0 ) return const1
     val const2 = log(mass) - logFactorial(fa.nItems)
     var sum = const1 + fa.nFeatures * const2
-    sum = -fa.array.groupBy(identity).map(_._2.length).foldLeft(0.0)((s, x) => s + logFactorial(x))
+    sum -= fa.array.groupBy(identity).map(_._2.length).foldLeft(0.0)((s, x) => s + logFactorial(x))
     sum += fa.sizes.foldLeft(0.0) { (s, mk) =>
       s + logFactorial(fa.nItems - mk) + logFactorial(mk - 1)
     }
