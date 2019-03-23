aa358298f214be76f48ebdecb7df13ded3e6907f
David B. Dahl <dahl@stat.byu.edu>
2019-03-23 07:21:54 -0600

    Yes

--- src/main/scala/org/ddahl/aibd/model/lineargaussian/FeatureAllocationDistributions.scala
+++ src/main/scala/org/ddahl/aibd/model/lineargaussian/FeatureAllocationDistributions.scala
@@ -7,6 +7,40 @@ import scala.collection.mutable.BitSet
 
 object FeatureAllocationDistributions {
 
+  def logProbabilityIBP(fa: FeatureAllocation, mass: Double): Double = {
+    val const1 = -mass * harmonicNumber(fa.nItems)
+    if ( fa.nFeatures == 0 ) return const1
+    val const2 = log(mass) - logFactorial(fa.nItems)
+    var sum = const1 + fa.nFeatures * const2
+    //sum -= computeRegardingTiesSlow(fa)
+    sum -= computeRegardingTies(fa)
+    sum += fa.sizes.foldLeft(0.0) { (s, mk) =>
+      s + logFactorial(fa.nItems - mk) + logFactorial(mk - 1)
+    }
+    sum
+  }
+
+  def computeRegardingTiesSlow(fa: FeatureAllocation): Double = {
+    fa.array.groupBy(identity).map(_._2.length).foldLeft(0.0)((s, x) => s + logFactorial(x))
+  }
+
+  def computeRegardingTies(fa: FeatureAllocation): Double = {
+    val aa = fa.array.zip(fa.sizes).sortWith(lessThan)
+    var sum = 0.0
+    var run = 1
+    var j = 1
+    while ( j < aa.length ) {
+      if ( compare(aa(j-1),aa(j)) == 0 ) run += 1
+      else {
+        sum += logFactorial(run)
+        run = 1
+      }
+      j += 1
+    }
+    sum += logFactorial(run)
+    sum
+  }
+
   def compare(x: (BitSet,Int), y: (BitSet, Int)): Int = {
     if ( x._2 < y._2 ) return -1
     else if ( x._2 > y._2 ) return 1
@@ -27,30 +61,5 @@ object FeatureAllocationDistributions {
 
   def lessThan(x: (BitSet,Int), y: (BitSet, Int)): Boolean = compare(x,y) <= 0
 
-  def logProbabilityIBP(fa: FeatureAllocation, mass: Double): Double = {
-    val const1 = -mass * harmonicNumber(fa.nItems)
-    if ( fa.nFeatures == 0 ) return const1
-    val const2 = log(mass) - logFactorial(fa.nItems)
-    var sum = const1 + fa.nFeatures * const2
-    //sum -= fa.array.groupBy(identity).map(_._2.length).foldLeft(0.0)((s, x) => s + logFactorial(x))
-    val aa = fa.array.zip(fa.sizes).sortWith(lessThan)
-    var sum2 = 0.0
-    var run = 1
-    var j = 1
-    while ( j < aa.length ) {
-      if ( compare(aa(j-1),aa(j)) == 0 ) run += 1
-      else {
-        sum2 += logFactorial(run)
-        run = 1
-      }
-      j += 1
-    }
-    sum2 += logFactorial(run)
-    sum -= sum2
-    sum += fa.sizes.foldLeft(0.0) { (s, mk) =>
-      s + logFactorial(fa.nItems - mk) + logFactorial(mk - 1)
-    }
-    sum
-  }
-
 }
+
